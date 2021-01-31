4b323f08c17113abbf952de25d1318e2162ae05d
ebisLab <ebaiden@techie.com>
2021-01-31 05:53:51 -0500

    title change to test deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29c3b4e667956546fff2";
+/******/ 	var hotCurrentHash = "7ddb221c70dfca8e49c9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,7 @@
 const Navigation =()=>{
 return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-<a className="navbar-brand" href="#">Navbar</a>
+<a className="navbar-brand" href="#">WooCommerce Shop</a>
 <div className="collapse navbar-collapse" id="navbarColor01">
   <ul className="navbar-nav mr-auto">
     <li className="nav-item active">
