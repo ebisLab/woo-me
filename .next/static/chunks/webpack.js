2613c2e8cb47f3db9247119e03eee37e80b7e8c3
ebisLab <ebaiden@techie.com>
2021-01-31 19:44:19 -0500

    product page2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d3db7b7abd97f78d2cea";
+/******/ 	var hotCurrentHash = "2594dc9c32dbd936a921";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
