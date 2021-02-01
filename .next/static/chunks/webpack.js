fc5c4ffb8222fd0b4aab748e63f91a1d126b3ffe
ebisLab <ebaiden@techie.com>
2021-02-01 17:19:14 -0500

    cart test page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ef0246144431885caf51";
+/******/ 	var hotCurrentHash = "0bd32c3aa86a8107b519";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/cart.js
@@ -0,0 +1,7 @@
+const CartHello=()=>{
+    return(
+        <div>
+            This is an empty cart come at me 
+        </div>
+    )
+}
